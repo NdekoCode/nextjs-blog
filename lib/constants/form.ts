394822f9bc7a738097babbfbc5f06d0b0c80d24d
Arick Bulakali <arickbulakali@gmail.com>
2024-11-17 21:10:28 +0200

    feat: add login form ui

---
+++ lib/constants/form.ts
@@ -0,0 +1,51 @@
+import { z } from 'zod';
+
+export const registerFormSchema = z
+  .object({
+    firstName: z
+      .string()
+      .min(2, {
+        message: "FirstName must be at least 2 character",
+      })
+      .max(255, {
+        message: "The ... must be lower or equal to 255 character",
+      }),
+    lastName: z
+      .string()
+      .min(2, {
+        message: "The lastName must be at least 2 character",
+      })
+      .max(255, {
+        message: "The ... must be lower or equal to 255 character",
+      }),
+    email: z.string().email(),
+    password: z.string().min(4),
+    confirmPassword: z.string().min(4),
+  })
+  .required()
+  .refine((formData) => formData.password !== formData.confirmPassword, {
+    message: "Password don't match",
+    path: ["confirmPassword"],
+  });
+
+export type RegisterFormType = z.infer<typeof registerFormSchema>;
+export const registerFormDefaultValue: RegisterFormType = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+export const loginFormSchema = z.object({
+  email: z.string().email({
+    message: "Provide a valid email address",
+  }),
+  password: z.string().min(4),
+}).required();
+
+export type LoginFormType = z.infer<typeof loginFormSchema>;
+export const loginFormDefaultValue: LoginFormType = {
+  email: "",
+  password: "",
+};
