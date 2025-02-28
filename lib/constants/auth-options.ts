974027df9b151b471c6bab47330117c2b38172fd
Arick Bulakali <arickbulakali@gmail.com>
2025-02-28 19:12:05 +0200

    feat: configure NextAuth with Prisma adapter and environment variables

--- lib/constants/auth-options.ts
+++ lib/constants/auth-options.ts
@@ -2,8 +2,12 @@ import { AuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
+import prisma from '@/lib/connect';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+
 export const AUTH_OPTIONS: AuthOptions = {
   debug: true,
+  adapter:PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: String(process.env.GITHUB_ID) || "",
